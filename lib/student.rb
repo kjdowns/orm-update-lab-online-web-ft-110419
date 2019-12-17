f71e2e15ec7032ee1daa112a5d09b4991e4e9a5c
Kevin Downs <kevin.downs04@gmail.com>
2019-12-17 17:09:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,14 @@ class Student
   end
   
   def self.create_table
+    sql = <<-SQL
+      CREATE TABLE students(
+        id INTEGER PRIMAY KEY,
+        name TEXT,
+        grade INTEGER)
+    SQL
     
+    DB[:conn].execute(sql)
   end
   
 end
